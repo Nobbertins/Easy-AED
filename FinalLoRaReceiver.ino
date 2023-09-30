8511277d5a76cd515a67a821864ce1edbeef4446
Nobbertins <77172442+Nobbertins@users.noreply.github.com>
2023-09-30 12:52:22 -0500

    Add files via upload
    receiver w/ HTTP Post

---
+++ FinalLoRaReceiver.ino
@@ -0,0 +1,133 @@
+#include "LoRaWan_APP.h"
+#include "Arduino.h"
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+#define RF_FREQUENCY                                915000000 // Hz
+
+#define TX_OUTPUT_POWER                             14        // dBm
+
+#define LORA_BANDWIDTH                              0         // [0: 125 kHz,
+                                                              //  1: 250 kHz,
+                                                              //  2: 500 kHz,
+                                                              //  3: Reserved]
+#define LORA_SPREADING_FACTOR                       7         // [SF7..SF12]
+#define LORA_CODINGRATE                             1         // [1: 4/5,
+                                                              //  2: 4/6,
+                                                              //  3: 4/7,
+                                                              //  4: 4/8]
+#define LORA_PREAMBLE_LENGTH                        8         // Same for Tx and Rx
+#define LORA_SYMBOL_TIMEOUT                         0         // Symbols
+#define LORA_FIX_LENGTH_PAYLOAD_ON                  false
+#define LORA_IQ_INVERSION_ON                        false
+
+
+#define RX_TIMEOUT_VALUE                            1000
+#define BUFFER_SIZE                                 30 // Define the payload size here
+
+char txpacket[BUFFER_SIZE];
+char rxpacket[BUFFER_SIZE];
+
+static RadioEvents_t RadioEvents;
+
+int16_t txNumber;
+
+int16_t rssi,rxSize;
+
+bool lora_idle = true;
+
+
+
+//works on 2.4 GHz connections only
+const char* ssid = "Austin 2.4 GHz";
+const char* password = "face2face";
+
+//Your Domain name with URL path or IP address with path
+String serverName = "https://ipp77fz2z8.execute-api.eu-north-1.amazonaws.com/Dev";
+String coords;
+// the following variables are unsigned longs because the time, measured in
+// milliseconds, will quickly become a bigger number than can be stored in an int.
+unsigned long lastTime = 0;
+// Timer set to 10 minutes (600000)
+//unsigned long timerDelay = 600000;
+// Set timer to 5 seconds (5000)
+unsigned long timerDelay = 5000;
+void setup() {
+    Serial.begin(115200);
+    Mcu.begin();
+    
+    txNumber=0;
+    rssi=0;
+  
+    RadioEvents.RxDone = OnRxDone;
+    Radio.Init( &RadioEvents );
+    Radio.SetChannel( RF_FREQUENCY );
+    Radio.SetRxConfig( MODEM_LORA, LORA_BANDWIDTH, LORA_SPREADING_FACTOR,
+                               LORA_CODINGRATE, 0, LORA_PREAMBLE_LENGTH,
+                               LORA_SYMBOL_TIMEOUT, LORA_FIX_LENGTH_PAYLOAD_ON,
+                               0, true, 0, 0, LORA_IQ_INVERSION_ON, true );
+      WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+ 
+  Serial.println("Timer set to 5 seconds (timerDelay variable), it will take 5 seconds before publishing the first reading.");
+}
+
+
+
+void loop()
+{
+  if(lora_idle)
+  {
+    lora_idle = false;
+    Serial.println("into RX mode");
+    Radio.Rx(0);
+  }
+  Radio.IrqProcess( );
+  //Send an HTTP POST request every 5 seconds
+  if ((millis() - lastTime) > timerDelay) {
+    //Check WiFi connection status
+    if(WiFi.status()== WL_CONNECTED){
+      HTTPClient http;
+
+      String serverPath = serverName;
+      
+      // Your Domain name with URL path or IP address with path
+      http.begin(serverPath.c_str());
+
+      http.addHeader("Content-Type", "application/json");
+      String postData = "{\"ID\":\"drone\",\"gps\":\""+coords+"\"}";
+      int httpResponseCode = http.POST(postData);
+      if(httpResponseCode != 200){
+      Serial.print("Error Code: ");
+      Serial.println(httpResponseCode);
+      }
+      else{
+      Serial.println("Data posted successfully");
+      }
+      // Free resources
+      http.end();
+    }
+    else {
+      Serial.println("WiFi Disconnected");
+    }
+    lastTime = millis();
+  }
+}
+void OnRxDone( uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr )
+{
+    rssi=rssi;
+    rxSize=size;
+    coords = rxpacket;
+    memcpy(rxpacket, payload, size );
+    rxpacket[size]='\0';
+    Radio.Sleep( );
+    Serial.printf("\r\nreceived packet \"%s\" with rssi %d , length %d\r\n",rxpacket,rssi,rxSize);
+    lora_idle = true;
+}
